7b54b1329113ef7ffa919640dd70f4f946a7d742
Viktor Bubanja <viktor.bubanja@hotmail.com>
2023-07-25 07:38:50 +0200

    Add API endpoint for saving the tracks in a playlist in Dynamodb

--- src/api.py
+++ src/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from src.spotify import get_playlists as get_spotify_playlists
+from src.spotify import get_playlists as get_spotify_playlists, get_tracks_in_playlist
+from src.dynamodb import DynamoDB
+from src.dynamodb_mapper import track_to_dynamodb
 
 app = FastAPI()
 
@@ -11,4 +13,12 @@ def get_playlists():
 
 @app.get("/playlists/{playlist_id}")
 def get_playlist(playlist_id: str):
-    return ""
+    return get_tracks_in_playlist(playlist_id)
+
+
+@app.post("/playlists/{playlist_id}/save")
+def save_playlist(playlist_id: str):
+    tracks = get_tracks_in_playlist(playlist_id)
+    db_tracks = [track_to_dynamodb(track) for track in tracks]
+    DynamoDB().save_items(db_tracks)
+    return "success"
